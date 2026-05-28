e01cb9fb55ef9b53eb8ff00ff023bd99534384c3
Sean Zamora <zamoraseans@gmail.com>
2026-05-27 21:55:45 -0700

    Added a search feature

--- src/main/java/com/pluralsight/services/Search.java
+++ src/main/java/com/pluralsight/services/Search.java
@@ -1,6 +1,18 @@
 package com.pluralsight.services;
 
+import com.pluralsight.models.Media;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Search
 {
-
+    public ArrayList<? extends Media> searchForMediaTitle(String input, ArrayList<? extends Media> mediaList)
+    {
+        ArrayList<? extends Media> firstThree = mediaList.stream()
+                .filter(media -> media.getTitle().toLowerCase().contains(input.toLowerCase()))
+                .limit(3)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return firstThree;
+    }
 }
